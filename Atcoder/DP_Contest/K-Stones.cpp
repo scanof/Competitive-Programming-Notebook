1d1512e54f38ed7af67b6d0f3e8e8a42d9f4dcff
scanof <35736537+scanof@users.noreply.github.com>
2020-02-18 15:41:24 -0500

    Create K-Stones.cpp

---
+++ Atcoder/DP_Contest/K-Stones.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0; i< (int)n; ++i)
+#define for1(i,n) for(int i=1; i<= (int)n; ++i)
+#define fore(i,l,r) for(int i=(int)l; i<= (int)r; ++i)
+#define ford(i,n) for(int i=(int)(n) - 1; i>= 0; --i)
+#define fored(i,l,r) for(int i=(int)r; i>= (int)l; --i)
+#define pb push_back
+#define el '\n'
+#define d(x) cout<< #x<< " " << x<<el
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<char,int> pci;
+typedef tuple<int, int, int> tiii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+
+const ll INF = LONG_LONG_MAX;
+const double PI = atan(1)*4;
+const int MAXN = 1e5+20;
+const double EPS= 1e-10;
+
+int dp[MAXN], n, k;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cout<< setprecision(2)<< fixed;
+    cin>> n>> k;
+    int arr[n];
+    forn(i,n){
+        cin>> arr[i];
+        dp[arr[i]]=1;
+    }
+    forn(i,k+1){
+        forn(j,n){
+            if(i-arr[j]>=0 && dp[i-arr[j]]==0){
+                dp[i]=1;
+            }
+        }
+    }
+    cout<< (dp[k] ? "First" : "Second" ) <<el;
+
+
+}
+
