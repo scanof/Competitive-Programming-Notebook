b19792aa2269005b5a377992b873f36b03cdc6a6
scanof <35736537+scanof@users.noreply.github.com>
2020-02-29 17:34:33 -0500

    Create Contains_Duplicate.cpp

---
+++ Leetcode/Contains_Duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mapa;
+        for(int i= 0; i< nums.size(); ++i){
+            mapa[nums[i]]++;
+            if(mapa[nums[i]]>1) return true;
+        }
+        return false;
+    }
+};
