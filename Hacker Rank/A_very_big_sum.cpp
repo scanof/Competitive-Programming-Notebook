6b41051adc68b33973bb48ccd122a1fcd47ed904
scanof <35736537+scanof@users.noreply.github.com>
2020-03-01 12:34:04 -0500

    Create A_very_big_sum.cpp

---
+++ Hacker Rank/A_very_big_sum.cpp
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static long aVeryBigSum(int n, long[] ar) {
+        // Complete this function
+        long suma=0;
+        for(int i=0;i<n;i++){
+            suma=suma+ar[i];
+               
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long[] ar = new long[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextLong();
+        }
+        long result = aVeryBigSum(n, ar);
+        System.out.println(result);
+    }
+}
