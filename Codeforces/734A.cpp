0f4024d8594da57c136e5c9be1d97ea9575f8877
scanof <35736537+scanof@users.noreply.github.com>
2020-03-01 17:39:28 -0500

    Create 734A.cpp

---
+++ Codeforces/734A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0; i< (int)n; ++i)
+#define for1(i,n) for(int i=1; i<= (int)n; ++i)
+#define fore(i,l,r) for(int i=(int)l; i<= (int)r; ++i)
+#define ford(i,n) for(int i=(int)(n) - 1; i>= 0; --i)
+#define fored(i,l,r) for(int i=(int)r; i>= (int)l; --i)
+#define pb push_back
+#define el '\n'
+#define d(x) cout<< #x<< " " << x<<el
+#define ri(n) scanf("%d",&n)
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<char,int> pci;
+typedef tuple<int, int, int> tiii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+
+const ll INF = LONG_LONG_MAX;
+const int MAXN = 1e5+200;
+const double PI = acos(-1);
+const double EPS= 1e-10;
+
+int dr[] = {1,-1,0, 0,1,-1,-1, 1}; // trick to explore an implicit 2D grid
+int dc[] = {0, 0,1,-1,1, 1,-1,-1};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cout<< setprecision(2)<< fixed;
+    int n;
+    cin>> n;
+    string s;
+    cin>> s;
+    int sum= 0;
+    forn(i,n){
+        if(s[i]=='A') sum++;
+        else --sum;
+    }
+    if(sum>0) cout<< "Anton"<<el;
+    else if(sum==0) cout<< "Friendship"<<el;
+    else cout<< "Danik"<<el;
+}
