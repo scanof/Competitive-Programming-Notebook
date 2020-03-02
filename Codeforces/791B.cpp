19e1870303d6dd25286af9bdd6151d6c139415d0
scanof <35736537+scanof@users.noreply.github.com>
2020-03-01 19:24:45 -0500

    Create 791B.cpp

---
+++ Codeforces/791B.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0; i< (int)n; ++i)
+#define for1(i,n) for(int i=1; i<= (int)n; ++i)
+#define fore(i,l,r) for(int i=(int)l; i<= (int)r; ++i)
+#define ford(i,n) for(int i=(int)(n) - 1; i>= 0; --i)
+#define fored(i,l,r) for(int i=(int)r; i>= (int)l; --i)
+#define pb push_back
+#define el '\n'
+#define d(x) cout<< #x<< " " << x<<el
+#define ri(n) scanf("%d",&n)
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<char,int> pci;
+typedef tuple<int, int, int> tiii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+
+const ll INF = LONG_LONG_MAX;
+const int MAXN = 2e5+200;
+
+vector<int> g[MAXN];
+bool seen[MAXN];
+ll sum = 0;
+ll comp = 0;
+void dfs(int u){
+    ++comp;
+    seen[u]= true;
+    sum+= g[u].size();
+    for(int v: g[u]){
+        if(!seen[v]){
+            dfs(v);
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cout<< setprecision(2)<< fixed;
+    int n, m, u, v;
+    cin>> n>> m;
+    forn(i,m){
+        cin>> u>> v;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+    for1(i,n){
+        if(!seen[i]){
+            sum= 0;
+            comp = 0;
+            dfs(i);
+            if(sum/2 != (comp-1)*comp/2){
+                cout<< "NO"<<el;
+                return 0;
+            }
+        }
+    }
+    cout<< "YES"<<el;
+}
+
