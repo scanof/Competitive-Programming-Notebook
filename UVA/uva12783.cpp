041014f77cb1af9923ff72f88280d25980b2351f
scanof <35736537+scanof@users.noreply.github.com>
2020-03-13 16:46:47 -0500

    Update uva12783.cpp

--- UVA/uva12783.cpp
+++ UVA/uva12783.cpp
@@ -1,80 +1,94 @@
 #include <bits/stdc++.h>
 
-#define forn(i,n) for(int i= 0; i<n; ++i)
-#define for1(i,n) for(int i= 1; i<=n; ++i)
-#define fore(i,l,r) for(int i= l; i<=r; ++i)
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0; i< (int)n; ++i)
+#define for1(i,n) for(int i=1; i<= (int)n; ++i)
+#define fore(i,l,r) for(int i=(int)l; i<= (int)r; ++i)
+#define ford(i,n) for(int i=(int)(n) - 1; i>= 0; --i)
+#define fored(i,l,r) for(int i=(int)r; i>= (int)l; --i)
 #define pb push_back
-#define el '\n';
-#define d(x) cout<< #x<< " "<< x<< el;
+#define el '\n'
+#define d(x) cout<< #x<< " " << x<<el
+#define ri(n) scanf("%d",&n)
 
 using namespace std;
 
 typedef long long ll;
-typedef vector<int> vi;
 typedef pair<int,int> ii;
+typedef pair<char,int> pci;
+typedef tuple<int, int, int> tiii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
 
+const ll INF = LONG_LONG_MAX;
+const int MAXN = 1e3+20;
+const double PI = acos(-1);
+const double EPS= 1e-10;
 
-vi g[1001];
-bool seen[1001];
-int timer, num[1001], low[1001];
+int dr[] = {1,-1,0, 0,1,-1,-1, 1}; // trick to explore an implicit 2D grid
+int dc[] = {0, 0,1,-1,1, 1,-1,-1};
+
+vi g[MAXN];
+int num[MAXN], low[MAXN];
+bool seen[MAXN];
+int n;
 vector<ii> ans;
-void tarjan(int v, int p) {
-    seen[v] = true;
-    num[v] = low[v] = timer++;
-    int children=0;
-    for(int to: g[v]) {
-        if(to == p) continue;
-        if(seen[to]) {
-            low[v] = min(low[v], num[to]);
-        } else {
-            tarjan(to, v);
-            low[v] = min(low[v], low[to]);
-            if(low[to] > num[v])
-                ans.push_back(ii(min(v, to),max(v, to) ));
+int timer;
+
+void dfs(int u, int p=-1){
+    seen[u]= true;
+    int children = 0;
+    low[u] = num[u]= timer++;
+    for(int v: g[u]){
+        if(v==p) continue;
+        if(seen[v]){
+            low[u]= min(low[v], low[u]);
+        }else{
+            dfs(v,u);
+            low[u]= min(low[v], low[u]);
+            if(low[v]> num[u]){
+                ans.pb(ii(min(u,v), max(u,v)));
+            }
+            children++;
         }
+    }
+    if(p==-1 && children>1){
+        //CUTPOINT
     }
 }
 
-
+void solve(){
+    ans.clear();
+    timer = 0;
+    memset(num, -1, sizeof num);
+    memset(seen, false, sizeof seen);
+    memset(low, -1, sizeof low);
+    forn(i,n){
+        if(!seen[i]) dfs(i);
+    }
+    sort(ans.begin(), ans.end());
+    cout<< ans.size();
+    for(ii x: ans){
+        cout<<" "<< x.fi<< " "<<x.se;
+    }
+    cout<<el;
+    forn(i,n) g[i].clear();
+}
 
 
 int main(){
-    ios_base:: sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
-    int n, m;
-    cin >> n >> m;
-    while(n!=0 && m!=0) {
-        timer  = 0;
-        memset(seen, false, sizeof seen);
-        memset(low, -1, sizeof low);
-        memset(num, -1, sizeof num);
-        int src, dst;
-
-        forn(i, m) {
-            cin >> src >> dst;
-            g[src].push_back(dst);
-            g[dst].pb(src);
+    int m;
+    while(cin>> n>> m && n+m!=0){
+        int u, v;
+        forn(i,m){
+            cin>> u>> v;
+            g[u].pb(v);
+            g[v].pb(u);
         }
-
-
-        forn(i, n) {
-            if(!seen[i])
-                tarjan(0, -1);
-        }
-        cout << ans.size();
-        sort(ans.begin(), ans.end());
-        for(ii a: ans) {
-            cout << " "<<a.first << " " << a.second;
-        }
-        cout << endl;
-        ans.clear();
-        forn(i,n) g[i].clear();
-        cin >> n >> m;
-
+        solve();
     }
+
 }
-
-
-
-
