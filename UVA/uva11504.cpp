0dd2114fc7f28fed8b8e3b47e3b32e3057e01840
scanof <35736537+scanof@users.noreply.github.com>
2020-03-18 16:00:35 -0500

    Create uva11504.cpp

---
+++ UVA/uva11504.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0; i< (int)n; ++i)
+#define for1(i,n) for(int i=1; i<= (int)n; ++i)
+#define fore(i,l,r) for(int i=(int)l; i<= (int)r; ++i)
+#define ford(i,n) for(int i=(int)(n) - 1; i>= 0; --i)
+#define fored(i,l,r) for(int i=(int)r; i>= (int)l; --i)
+#define pb push_back
+#define el '\n'
+#define d(x) cout<< #x<< " " << x<<el
+#define ri(n) scanf("%d",&n)
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<char,int> pci;
+typedef tuple<int, int, int> tiii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+
+const ll INF = LONG_LONG_MAX;
+const int MAXN = 1e5+200;
+const double PI = acos(-1);
+const double EPS= 1e-10;
+
+vector<int> g[MAXN];
+bool seen[MAXN];
+vector<int> ts;
+
+void dfs1(int u){
+    seen[u]= true;
+    for(int v: g[u]){
+        if(!seen[v]){
+            dfs1(v);
+        }
+    }
+    ts.pb(u);
+}
+
+void dfs(int u){
+    seen[u]= true;
+    for(int v: g[u]){
+        if(!seen[v]){
+           dfs(v);
+        }
+    }
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cout<< setprecision(20)<< fixed;
+    int t;
+    cin>> t;
+    int n, m;
+    int u, v;
+    while(t--){
+        cin>> n>> m;
+        forn(i,n) g[i].clear();
+        memset(seen, false, sizeof seen);
+        ts.clear();
+        forn(i,m){
+            cin>> u>> v;
+            g[--u].pb(--v);
+        }
+        forn(i,n){
+            if(!seen[i]){
+                dfs1(i);
+            }
+        }
+        memset(seen, false, sizeof seen);
+        int ans = 0;
+        reverse(ts.begin(), ts.end());
+        for(int i : ts){
+            if(!seen[i]){
+                dfs(i); ++ans;
+            }
+        }
+        cout<< ans<<el;
+    }
+}
+
