386d168ed3f95e431c9583ae9c8bebfa4796ad90
scanof <35736537+scanof@users.noreply.github.com>
2020-03-19 14:43:19 -0500

    Update Coin_Change.cpp

--- Leetcode/Coin_Change.cpp
+++ Leetcode/Coin_Change.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
+    const int INF = 1e9;
     int coinChange(vector<int>& coins, int amount) {
-        int n= coins.size();
         int dp[amount+1];
-        for(int i=0;i<=amount;++i) dp[i]= INT_MAX;
         dp[0]=0;
-        for(int x: coins) 
-            if(x<=amount) dp[x]=1;
-        for(int i= 0; i<=amount; ++i)
-            for(int x: coins)
-                if(i-x>=0 && dp[i-x]!=INT_MAX)
-                    dp[i]= min(dp[i], dp[i-x]+1);
-        for(int i= 0; i<= amount; ++i){
-            cout<< dp[i]<< " ";
+        for(int i=1; i<=amount; ++i){
+            dp[i]=INF;
+            for(int coin: coins){
+                if(i-coin<0) continue;
+                dp[i]= min(dp[i], 1+ dp[i-coin]);
+            }
         }
-        cout<<endl;
-        return (dp[amount]==INT_MAX ? -1 : dp[amount]);
+        return (dp[amount]==INF ? -1 : dp[amount]);
     }
 };
