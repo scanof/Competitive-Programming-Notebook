ce5b81b5a64d1017cb11facd1f23813f11c92abb
scanof <35736537+scanof@users.noreply.github.com>
2020-04-07 04:01:28 -0500

    Create 996A.cpp

---
+++ Codeforces/996A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0; i< (int)n; ++i)
+#define for1(i,n) for(int i=1; i<= (int)n; ++i)
+#define fore(i,l,r) for(int i=(int)l; i<= (int)r; ++i)
+#define ford(i,n) for(int i=(int)(n) - 1; i>= 0; --i)
+#define fored(i,l,r) for(int i=(int)r; i>= (int)l; --i)
+#define pb push_back
+#define el '\n'
+#define d(x) cout<< #x<< " " << x<<el
+#define ri(n) scanf("%d",&n)
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef pair<char,int> pci;
+typedef tuple<int, int, int> tiii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int n;
+  cin>> n;
+  cout<< n/100 + (n%100)/20 + (n%20)/10 + (n%10)/5 +(n%5)<<el;
+}
+
+
+
+
