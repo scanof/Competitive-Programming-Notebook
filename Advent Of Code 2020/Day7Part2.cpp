01b568114828d8b9525883eb875fc74f5db70d4e
scanof <35736537+scanof@users.noreply.github.com>
2020-12-07 12:19:25 -0500

    Create Day7Part2.cpp

---
+++ Advent Of Code 2020/Day7Part2.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0; i< (int)n; ++i)
+#define for1(i,n) for(int i=1; i<= (int)n; ++i)
+#define fore(i,l,r) for(int i=(int)l; i<= (int)r; ++i)
+#define ford(i,n) for(int i=(int)(n) - 1; i>= 0; --i)
+#define fored(i,l,r) for(int i=(int)r; i>= (int)l; --i)
+#define pb push_back
+#define el '\n'
+#define d(x) cout<< #x<< " " << x<<el
+#define ri(n) scanf("%d",&n)
+#define sz(v) ((int)v.size())
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef pair<int,int> ii;
+typedef pair<char,int> pci;
+typedef tuple<int, int, int> tiii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+
+const int inf = 1e9;
+const int nax = 1e3+200;
+const ld pi = acos(-1);
+const ld eps= 1e-9;
+
+int dr[] = {1,-1,0, 0,1,-1,-1, 1}; // trick to explore an implicit 2D grid
+int dc[] = {0, 0,1,-1,1, 1,-1,-1};
+
+unordered_map<string, int> mp;
+int idx = 0;
+
+int ans = 0;
+vector<ii> g[nax];
+bool seen[nax];
+ll szs[nax];
+
+void dfs(int u){
+  szs[u] = 1;
+  seen[u] =  true;
+  for(ii e: g[u]){
+    int v, w;
+    tie(v,w) = e;
+    if(!seen[v]){
+      dfs(v);
+    }
+    szs[u] += 1LL*w*szs[v];
+  }
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  string tmp;
+  while(getline(cin, tmp)){
+    stringstream ss(tmp);
+    string s1, s2, x;
+    ss >> s1 >> s2 >> x;
+    assert(x=="bags");
+    ss>> x;
+    s1 += " "+ s2;
+    int u;
+    if(!mp.count(s1)) mp[s1] = idx++;
+    u = mp[s1];
+    string num;
+    while(ss >> num){
+      if(num!="no") ss >> s1 >> s2 >> x;
+      else{
+        ss >> s1>> s2;
+        continue;
+      }
+      s1 += " "+ s2;
+      int v;
+      int val = stoi(num);
+      if(!mp.count(s1)) mp[s1] = idx++;
+      v = mp[s1];
+      g[u].pb({v,val});
+    }
+  }
+  int u = mp["shiny gold"];
+  dfs(u);
+  cout<< "answer : " << szs[mp["shiny gold"]] - 1 <<el;
+}
