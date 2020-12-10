618d1de4b5fabf5a29fa75a2d0c6284a909be1f5
Santiago Cano <35736537+scanof@users.noreply.github.com>
2020-12-10 15:42:10 -0500

    Add files via upload

---
+++ Advent Of Code 2020/Day10_part1.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+#define forn(i,n) for(int i=0; i< (int)n; ++i)
+#define for1(i,n) for(int i=1; i<= (int)n; ++i)
+#define fore(i,l,r) for(int i=(int)l; i<= (int)r; ++i)
+#define ford(i,n) for(int i=(int)(n) - 1; i>= 0; --i)
+#define fored(i,l,r) for(int i=(int)r; i>= (int)l; --i)
+#define pb push_back
+#define el '\n'
+#define d(x) cout<< #x<< " " << x<<el
+#define ri(n) scanf("%d",&n)
+#define sz(v) ((int)v.size())
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef pair<int,int> ii;
+typedef pair<char,int> pci;
+typedef tuple<int, int, int> tiii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+
+const int inf = 1e9;
+const int nax = 1e5+200;
+const ld pi = acos(-1);
+const ld eps= 1e-9;
+
+int dr[] = {1,-1,0, 0,1,-1,-1, 1}; // trick to explore an implicit 2D grid
+int dc[] = {0, 0,1,-1,1, 1,-1,-1};
+
+int a[nax];
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  cout<< setprecision(20)<< fixed;
+  int mx = 0, x, last = 0, uno = 0, tres = 0;
+  while(cin>> x){
+    mx = max(mx, x);
+    a[x]++;
+  }
+  fore(i,0,mx){
+    if(a[i]){
+      if(i-last==1) ++uno;
+      else if(i-last==3) ++tres;
+      last = i;
+    }
+  }
+  tres++;
+  cout<< uno*tres<<el;
+}
