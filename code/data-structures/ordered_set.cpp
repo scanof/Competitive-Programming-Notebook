2e9f9059588ddbc9ed7c35efab1c4c51d63a042a
Juan Camilo Salazar <63207344+juan-c-s@users.noreply.github.com>
2024-03-11 22:37:48 -0500

    Update ordered_set.cpp

--- code/data-structures/ordered_set.cpp
+++ code/data-structures/ordered_set.cpp
@@ -2,9 +2,14 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 typedef tree<int, null_type, less<int>, rb_tree_tag,
-tree_order_statistics_node_update> ordered_set; 
-// Para multiset cambiar less<int> por less_equal<int>
-// METHODS
-tree.find_by_order(k) // returns pointer to the k-th smallest element
-tree.order_of_key(x)  // returns how many elements are smaller than x
-tree.find_by_order(k) == tree.end() // true, if element does not exist
+tree_order_statistics_node_update> ordered_set;
+// -------- CONSTRUCTOR -------- //
+// 1. Para ordenar por MAX cambiar less<int> por greater<int>
+// 2. Para multiset cambiar less<int> por less_equal<int>
+//      Para borrar siendo multiset:
+//      int idx = st.order_of_key(value);
+//      st.erase(st.find_by_order(idx));
+// -------- METHODS --------- //
+st.find_by_order(k) // returns pointer to the k-th smallest element
+st.order_of_key(x)  // returns how many elements are smaller than x
+st.find_by_order(k) == st.end() // true, if element does not exist
